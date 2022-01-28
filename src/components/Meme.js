d990d117a1d825b4963323e99bc4c35357097506
maurokein <maurokein1990@gmail.com>
2022-01-28 12:39:26 -0300

    some minor changes added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import memesData from '../memesData'
 
 export default function Meme(){
+    function getRandomMeme() {
+        const memesArray = memesData.data.memes;
+        const randomIndex = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomIndex].url;
+        console.log(url);
+    }   
     return(
         <main className='form'>
             <div className='form--input'>
@@ -8,7 +15,7 @@ export default function Meme(){
                 <input type='text' name='input' id='punchline' placeholder='Bottom text' required className='form--secondinput'></input>
             </div>
             <div className='form--submit'>
-                <button type='submit' className='form--submit__input'>Get a new meme image<i class="fa fa-picture-o" aria-hidden="true"></i></button>
+                <button type='submit' className='form--submit__input' onClick={getRandomMeme}>Get a new meme image<i class="fa fa-picture-o" aria-hidden="true"></i></button>
             </div>
         </main>
     )
