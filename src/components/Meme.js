acfc56ae59620ab386c55bb84fa8f1a533797997
maurokein <maurokein1990@gmail.com>
2022-02-01 12:36:50 -0300

    random image added to meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,11 @@ import React from 'react'
 import memesData from '../memesData'
 
 export default function Meme(){
+    const [memeImageUrl, setMemeImageUrl] = React.useState("");
     function getRandomMeme() {
         const memesArray = memesData.data.memes;
         const randomIndex = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomIndex].url;
-        console.log(url);
+        setMemeImageUrl(memesArray[randomIndex].url);
     }   
     return(
         <main className='form'>
@@ -17,6 +17,7 @@ export default function Meme(){
             <div className='form--submit'>
                 <button type='submit' className='form--submit__input' onClick={getRandomMeme}>Get a new meme image<i class="fa fa-picture-o" aria-hidden="true"></i></button>
             </div>
+            <img src={memeImageUrl} alt='meme' className='form--image' />
         </main>
     )
 }
